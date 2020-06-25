4bbbd44a39844236254bf2b3d913eb53c34866d2
tommyapk <tommyapk24@gmail.com>
2020-06-25 19:54:08 +0800

    tambah topup activity

---
+++ app/src/main/java/com/example/iporter2/activity/TopUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.iporter2.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.iporter2.R;
+
+public class TopUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_up);
+    }
+}
